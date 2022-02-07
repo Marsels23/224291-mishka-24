6fb0463dad3c121e39639338b3d97c4bcd79677c
Марина Лопатникова <marinal2303@yandex.ru>
2022-02-07 19:25:12 +0300

    Правки_2

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ const html = () => {
 // Scripts
 
 const scripts = () => {
-  return gulp.src('source/js/script.js')
+  return gulp.src('source/js/app.js')
     .pipe(gulp.dest('build/js'))
     .pipe(browser.stream());
 }
